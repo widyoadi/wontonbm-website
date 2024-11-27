1e1daeac67250503cf5ac5f224e63dab8773a9fa
widyo <widyo.adi.octavianto@gmail.com>
2024-11-27 10:59:32 +0700

    update gambar

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -1,9 +1,15 @@
 <main class="mt-[70px] pt-12 px-12 max-w-screen-xl">
   <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lobortis
-    urna non est pharetra pharetra. In a dignissim arcu. Phasellus et porta
-    eros, vel iaculis dui. In eu quam convallis, ultrices nibh in, vulputate
-    velit. Aliquam erat volutpat. Aenean nec viverra enim. Orci varius natoque
-    penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+    Catatan sejarah menyebutkan bahwa sajian ini sudah ada sejak masa Dinasti Tang (618-906 M). Oleh masyarakat Tiongkok kuno, wonton hampir disamakan dengan jiaozi.
+    Namun, perkembangan wonton kemudian membuatnya menjadi berbagai jenis masakan sehingga orang-orang pada masa selanjutnya mulai membedakan keduanya.
+    Sekilas jika diperhatikan bentuk wonton enggak jauh berbeda dengan jiaozi tapi isiannya biasanya lebih sedikit dan dibentuk menjadi segitiga.
+    Berbeda dengan jiaozi yang dibungkus dnegan kulit yang tebal dan isiannya yang lebih besar dengan tepi yang dibuat berlipat.
+    Dulunya wonton digunakan sebagai sajian yang berfungsi untuk menyehatkan tubuh karena diisi dengan berbagai bahan-bahan herbal.
+    Kala itu wonton banyak dikonsumsi oleh orang-orang yang cukup kesulitan menelan obat.
+    Sajian ini baru mulai berkembang ke luar Tiongkok pasca Perang Dunia II berakhir dan kondisi ekonomi di Tiongkok sudah mulai membaik.
+    Hongkong disebut sebagai negara pertama yang memperkenalkan wonton sebagai hidangan pekerja yang dijajakan di kios-kios kecil.
+    Hidangan ini berubah menjadi sangat populer karena cita rasanya yang gurih dan kuahnya yang hangat.
+    Itulah tadi uraian tentang sejarah wonton atau pangsit yang populer di Indonesia sebagai camilan pangsit goreng.
+    Beberapa varian pangsit yang populer di Indonesia biasanya dijual dengan isian atau bisa juga hanya kulitnya saja, dan dikonsumsi bersama sajian berkuah seperti bakso atau mie ayam.
   </p>
 </main>
